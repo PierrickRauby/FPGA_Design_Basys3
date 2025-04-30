d70b36d745ee5a42a5649580b41e4d317fcbe66f
PierrickRauby <pierrick.rauby@gadz.org>
2025-04-30 11:57:31 -0400

    updated bit file path in tcl script to be relative

--- program_fpga.tcl
+++ program_fpga.tcl
@@ -12,7 +12,7 @@ refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a35t_0] 0]
 
 set_property PROBES.FILE {} [get_hw_devices xc7a35t_0]
 set_property FULL_PROBES.FILE {} [get_hw_devices xc7a35t_0]
-set_property PROGRAM.FILE {/home/media/Documents/FPGA_Design_Basys3/top.bit} [get_hw_devices xc7a35t_0]
+set_property PROGRAM.FILE {./top.bit} [get_hw_devices xc7a35t_0]
 program_hw_devices [get_hw_devices xc7a35t_0]
 refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0]
 
